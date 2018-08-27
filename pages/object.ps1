b3881ac578f36c08e4dd9119a8d5f16d7b110167
tobias.mueller@schriesheim.de <29426934+MuellerSchriesheim@users.noreply.github.com>
2018-08-27 15:28:02 +0200

    Bugfix to get Buttons working Enable, Disable and Removing weren't working because of the wrong -Identity Parameter. Also the Enable/Disable Button is now properly updated after an Click. Previously the function didn't get an update about the current status of the AD object.

--- pages/object.ps1
+++ pages/object.ps1
@@ -1,17 +1,21 @@
 New-UDPage -Url "/object/:identity" -Endpoint {
-    param($identity) 
+    param($identity)
 
     $Object = Get-ADObject -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties ObjectClass -IncludeDeletedObjects
-    if ($Object.ObjectClass -eq "user") {
-        $Object = Get-ADUser -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties * 
+    if ($Object.ObjectClass -eq "user")
+    {
+        $Object = Get-ADUser -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties *
     }
-    elseif ($Object.ObjectClass -eq "computer") {
+    elseif ($Object.ObjectClass -eq "computer")
+    {
         $Object = Get-ADComputer -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties *
     }
-    elseif ($Object.ObjectClass -eq "group") {
-        $Object = Get-ADGroup -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties * 
+    elseif ($Object.ObjectClass -eq "group")
+    {
+        $Object = Get-ADGroup -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties *
     }
-    else {
+    else
+    {
         $Object = Get-ADObject -Filter { Name -eq $Identity } @Cache:ConnectionInfo -Properties * -IncludeDeletedObjects
     }
 
@@ -27,21 +31,22 @@ New-UDPage -Url "/object/:identity" -Endpoint {
                         New-UDHeading -Size 5 -Text $Object.SamAccountName
                     }
                 }
-            } 
+            }
         }
 
-        if (-not $Object.Deleted) {
+        if (-not $Object.Deleted)
+        {
             New-UDColumn -Size 4 -Content {
                 New-UDElement -Tag "div" -Attributes @{ style = @{ height = "10px"}}
                 New-UDButton -Icon trash -Text "Delete" -OnClick {
                     Show-UDModal -Header { New-UDHeading -Size 5 -Text "Are you sure you want to delete this object?" } -Content {
 
-                        New-UDHeading -Size 5 -Text "Clicking ok will run: Remove-ADObject -Identity $identity -Confirm:`$false"
+                        New-UDHeading -Size 5 -Text "Clicking ok will run: Remove-ADObject -Identity $($Object.samAccountName) -Confirm:`$false"
 
                         New-UDRow -Columns {
                             New-UDColumn -Size 2 -Content {
                                 New-UDButton -Text "Ok" -OnClick {
-                                    Remove-ADObject -Identity $identity @Cache:ConnectionInfo -Confirm:$false
+                                    Remove-ADObject -Identity $Object.samAccountName @Cache:ConnectionInfo -Confirm:$false
                                     Hide-UDModal
                                     Invoke-UDRedirect -Url "/home"
                                 } -Icon warning
@@ -52,42 +57,47 @@ New-UDPage -Url "/object/:identity" -Endpoint {
                                 }
                             }
                         }
-                    } 
+                    }
                 }
 
-                if ($Object.ObjectClass -eq "user" -or $Object.ObjectClass -eq "computer") {
+                if ($Object.ObjectClass -eq "user" -or $Object.ObjectClass -eq "computer")
+                {
                     $EnabledText = "Enable"
                     $btnIcon = 'check'
-                    if ($Object.Enabled) {
+                    if ($Object.Enabled)
+                    {
                         $EnabledText = "Disable"
                         $btnIcon = 'xing'
                     }
 
                     New-UDElement -Tag "div" -Attributes @{ style = @{ height = "10px"}} -Content {}
                     New-UDButton -Id "btnEnabled" -Icon $btnIcon -Text $EnabledText  -OnClick {
-                        if ($Object.Enabled) {
-                            Disable-ADAccount -Identity $identity @Cache:ConnectionInfo
+                        if ($Object.Enabled)
+                        {
+                            $Object.Enabled = $(Disable-ADAccount -Identity $Object.samAccountName @Cache:ConnectionInfo -PassThru).Enabled
                             Set-UDElement -Id "btnEnabled" -Content {
                                 New-UDIcon -Icon 'check'
                                 "Enable"
                             }
                         }
-                        else {
-                            Enable-ADAccount -Identity $identity @Cache:ConnectionInfo
+                        else
+                        {
+                            $Object.Enabled = $(Enable-ADAccount -Identity $Object.samAccountName @Cache:ConnectionInfo -PassThru).Enabled
                             Set-UDElement -Id "btnEnabled" -Content {
                                 New-UDIcon -Icon 'xing'
                                 "Disable"
                             }
                         }
                     }
-                }             
+                }
             }
         }
 
 
     }
 
-    if ($Object.ObjectClass -eq 'user') {
+    if ($Object.ObjectClass -eq 'user')
+    {
         New-UDRow -Columns {
             New-UDColumn -SmallSize 12 -Content {
                 New-UDCollapsible -Items {
@@ -96,12 +106,14 @@ New-UDPage -Url "/object/:identity" -Endpoint {
                             New-UDInputField -Name "Password" -Placeholder "Password" -Type "password"
                         } -Endpoint {
                             param($Password)
-        
-                            try {
+
+                            try
+                            {
                                 Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText -String $Password -Force) -Identity $identity @Cache:ConnectionInfo
                                 New-UDInputAction -Toast "Password Reset" -Duration 3000
                             }
-                            catch {
+                            catch
+                            {
                                 New-UDInputAction -Toast "$_" -Duration 3000
                             }
                         }
@@ -111,7 +123,8 @@ New-UDPage -Url "/object/:identity" -Endpoint {
         }
     }
 
-    if ($Object.ObjectClass -eq 'group') {
+    if ($Object.ObjectClass -eq 'group')
+    {
         New-UDRow -Columns {
             New-UDColumn -SmallSize 12 -Content {
                 New-UDCollapsible -Items {
@@ -120,9 +133,9 @@ New-UDPage -Url "/object/:identity" -Endpoint {
                             Get-ADGroupMember -Identity $identity @Cache:ConnectionInfo | ForEach-Object {
                                 $member = $_
                                 [PSCustomObject]@{
-                                    Name = $_.name
+                                    Name   = $_.name
                                     Remove = New-UDButton -Text "Remove" -OnClick {
-                                        Remove-ADGroupMember -Identity $identity @Cache:ConnectionInfo -Members $member -Confirm:$false 
+                                        Remove-ADGroupMember -Identity $identity @Cache:ConnectionInfo -Members $member -Confirm:$false
                                     }
                                 }
                             } | Out-UDTableData -Property @("Name", "Remove")
@@ -141,26 +154,30 @@ New-UDPage -Url "/object/:identity" -Endpoint {
 
                 $Object.psobject.Properties | ForEach-Object {
 
-                    if ($SkippedProperties.Contains( $_.Name)) {
+                    if ($SkippedProperties.Contains( $_.Name))
+                    {
                         return
                     }
 
                     $Value = $Null
-                    if ($_.Value -eq $null) {
+                    if ($_.Value -eq $null)
+                    {
                         $Value = ' '
                     }
-                    elseif ($_.Value -is [Microsoft.ActiveDirectory.Management.ADPropertyValueCollection]) {
-                        $Value = ($_.Value | ForEach-Object { 
-                            $_.ToString()
-                            New-UDElement -Tag "br"
-                        }) 
+                    elseif ($_.Value -is [Microsoft.ActiveDirectory.Management.ADPropertyValueCollection])
+                    {
+                        $Value = ($_.Value | ForEach-Object {
+                                $_.ToString()
+                                New-UDElement -Tag "br"
+                            })
                     }
-                    else {
+                    else
+                    {
                         $Value = $_.Value.ToString()
                     }
 
                     [PSCustomObject]@{
-                        Name = $_.Name 
+                        Name  = $_.Name
                         Value = $Value
                     } | Out-UDTableData -Property @("Name", "Value")
                 }
